07947ac9aab974d4b7fb679c3828474febd74c33
jonDuke <jonduke90@gmail.com>
2020-07-13 11:41:10 -0700

    implemented stack with a python list

--- stack/stack.py
+++ stack/stack.py
@@ -13,13 +13,22 @@ return elements in Last In First Out order.
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return len(self.storage)
 
     def push(self, value):
-        pass
+        self.storage.append(value)
 
     def pop(self):
-        pass
+        if len(self.storage) > 0:
+            return self.storage.pop(-1)
+        else:
+            return None
+    
+    def peek(self):
+        if len(self.storage) > 0:
+            return self.storage[-1]
+        else:
+            return None
